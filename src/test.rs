4751bcd36dcbab551655ff0ec0953b9c65810ce7
ELF-Nigel <ELF-Nigel@users.noreply.github.com>
2026-03-02 09:23:41 -0500

    Ignore networked tests in CI

--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,5 @@
 #[test]
+#[ignore]
 fn bench2() {
     let mut keyauth = crate::v1_2::KeyauthApi::new("library-development", "EdmsTKiuld", "9f752b6a414455175efd942abfd2183667413d57b1d59d6742d8437c71802b49", "1.0", "https://keyauth.win/api/1.2/");
     let _ = keyauth.init(None);
@@ -7,6 +8,7 @@ fn bench2() {
 
 #[test]
 #[cfg(feature = "v1_0")]
+#[ignore]
 fn bench1() {
     let mut keyauth = crate::v1_0::KeyauthApi::new("library-development", "EdmsTKiuld", "9f752b6a414455175efd942abfd2183667413d57b1d59d6742d8437c71802b49", "1.0", "https://keyauth.win/api/1.0/");
     let _ = keyauth.init(None);
